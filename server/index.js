646dcc39a6ac8fc2906ed41f789d56016a919d2b
Yurii <94929434+Yurii-Sapsai@users.noreply.github.com>
2022-06-28 22:02:45 +0300

    Added functionality for uploading a file to the server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from 'multer';
 
 
 import { registerValidation, loginValidation } from './validations/auth.js'
@@ -15,6 +16,15 @@ import * as PostController from './controllers/PostController.js'
 const app = express()
 app.use(express.json())
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+const upload = multer({ storage })
 
 
 app.get('/', (req, res) => {
@@ -22,12 +32,18 @@ app.get('/', (req, res) => {
 })
 
 app.post('/auth/register', registerValidation, UserController.register)
-app.post('/auth/login', loginValidation , UserController.login)
+app.post('/auth/login', loginValidation, UserController.login)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
-app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
+
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
 app.patch('/posts/:id', checkAuth, PostController.update)
 
