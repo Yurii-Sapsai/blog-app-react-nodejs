d2c573812dfeab31983254362f6c6cb8d955caae
Yurii <94929434+Yurii-Sapsai@users.noreply.github.com>
2022-06-30 13:23:36 +0300

    Refactoring the code of components Нeader and Login on client

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,10 +4,20 @@ import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 
-export const Header = () => {
-  const isAuth = false;
+import {useDispatch, useSelector} from 'react-redux'
+import { logout, selectIsAuth } from "../../redux/slices/auth";
 
-  const onClickLogout = () => {};
+export const Header = () => {
+
+  const isAuth = useSelector(selectIsAuth)
+  const dispatch = useDispatch()
+
+  const onClickLogout = () => {
+    if(window.confirm('Вы действительно хотите выйти?')){
+      dispatch(logout())
+      window.localStorage.removeItem('token')
+    }
+  };
 
   return (
     <div className={styles.root}>
---
+++ client/src/redux/slices/auth.js
@@ -0,0 +1,58 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from '../../axios';
+
+export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
+    const {data} = await axios.post('/auth/login', params)
+    return data
+})
+
+export const fetchAuthMe = createAsyncThunk('auth/fetchAuthMe', async () => {
+    const {data} = await axios.get('/auth/me')
+    return data
+})
+
+const initialState = {
+    data: null,
+    status: 'loading',
+}
+
+const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        logout: (state) => {
+            state.data = null
+            state.status = 'loaded'
+        }
+    },
+    extraReducers: {
+        [fetchAuth.pending]: (state) => {
+            state.data = null
+        },
+        [fetchAuth.fulfilled]: (state, action) => {
+            state.data = action.payload
+            state.status = 'loaded'
+        },
+        [fetchAuth.rejected]: (state) => {
+            state.data = null
+            state.status = 'error'
+        },
+        [fetchAuthMe.pending]: (state) => {
+            state.data = null
+        },
+        [fetchAuthMe.fulfilled]: (state, action) => {
+            state.data = action.payload
+            state.status = 'loaded'
+        },
+        [fetchAuthMe.rejected]: (state) => {
+            state.data = null
+            state.status = 'error'
+        }
+    }
+})
+
+export const selectIsAuth = (state) => Boolean(state.auth.data)
+
+export const {logout} = authSlice.actions
+
+export const authReducer  = authSlice.reducer
